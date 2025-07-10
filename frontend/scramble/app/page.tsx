ff21150fd737c2dd4c539fbe1eca1f1360ddc618
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2025-07-10 00:22:47 -0700

    auth0 scuffed

--- frontend/scramble/app/page.tsx
+++ frontend/scramble/app/page.tsx
@@ -1,30 +1,55 @@
+"use client"
+
 import CreateGameButton from "@/components/page/CreateGameButton";
 import FindGameButton from "@/components/page/FindGameButton";
 import { JoinGameForm } from "@/components/page/JoinGameForm";
+import { useUser } from "@auth0/nextjs-auth0";
 
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-4">
-      <h1 className="text-4xl font-bold mb-8">welcome to my game</h1>
+    const { user, isLoading } = useUser();
 
-      <div className="flex space-x-4 mb-12">
-        <CreateGameButton></CreateGameButton>
-        <FindGameButton></FindGameButton>
-      </div>
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center p-4">
+            <h1 className="text-4xl font-bold mb-8">welcome to my game</h1>
 
-      {/* <h1 className="text-2xl font-semibold mb-6">Have a game code already?</h1> */}
+            <div className="flex space-x-4 mb-12">
+                <CreateGameButton></CreateGameButton>
+                <FindGameButton></FindGameButton>
+            </div>
 
-      <div className="flex space-x-4">
-        {/* <input
+            {/* <h1 className="text-2xl font-semibold mb-6">Have a game code already?</h1> */}
+
+            <div className="flex space-x-4">
+                {/* <input
           type="text"
           placeholder="Enter game code"
           className="px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <button className="px-6 py-3 rounded-lg shadow-md">Join Game</button> */}
-        <JoinGameForm></JoinGameForm>
-      </div>
-    </div>
-  );
+                <JoinGameForm></JoinGameForm>
+            </div>
+            <div className="flex space-x-4">
+                <a href="/auth/login">Login</a>
+                <a href="/auth/logout">Logout</a>
+            </div>
+
+            <>
+                {isLoading && <p>Loading...</p>}
+                {user && (
+                    <div style={{ textAlign: "center" }}>
+                        <img
+                            src={user.picture}
+                            alt="Profile"
+                            style={{ borderRadius: "50%", width: "80px", height: "80px" }}
+                        />
+                        <h2>{user.name}</h2>
+                        <p>{user.email}</p>
+                        <pre>{JSON.stringify(user, null, 2)}</pre>
+                    </div>
+                )}
+            </>
+        </div>
+    );
 }
